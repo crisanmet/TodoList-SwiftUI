fa3491177401d65531ae7c20cb161e2e2b94ba7d
Cristian Sancricca <cristiansancricca@gmail.com>
2022-07-16 00:32:00 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Cristian Sancricca on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
